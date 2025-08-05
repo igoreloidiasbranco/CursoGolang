4aee555ab9b88ff8b12e5aa8777bd64825abfdf8
Igor Eloi <igor.dbranco@mercadolivre.com>
2025-08-05 20:27:54 -0300

    primeiros comandos Go

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,11 @@
+// pacote principal para executar o programa
+package main
+
+// import do pacote fmt disponibilizado pela linguagem Go
+import "fmt"
+
+// função principal(main) para executar o programa
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa!")
+}
