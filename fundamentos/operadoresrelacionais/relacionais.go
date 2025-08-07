5cd9af36afd3ace91316fc905b3944b0ff7e13e8
Igor Eloi <igor.dbranco@mercadolivre.com>
2025-08-07 20:36:13 -0300

    operadores relacionais

---
+++ fundamentos/operadoresrelacionais/relacionais.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	fmt.Println("String:", "Banana" == "Banana")
+	fmt.Println("!=", 3 != 2)
+	fmt.Println("<", 3 < 2)
+	fmt.Println(">", 3 > 2)
+	fmt.Println("<=", 3 <= 2)
+	fmt.Println(">=", 3 >= 2)
+
+	d1 := time.Unix(0, 0)
+	d2 := time.Unix(0, 0)
+
+	fmt.Println(d1)
+	fmt.Println(d2)
+
+	fmt.Println("Datas:", d1 == d2) // compara valores e não a referência
+	fmt.Println("Datas:", d1.Equal(d2))
+
+	type Pessoa struct {
+		Nome string
+	}
+
+	p1 := Pessoa{"João"}
+	p2 := Pessoa{"João"}
+
+	fmt.Println("Pessoas:", p1 == p2)
+}
