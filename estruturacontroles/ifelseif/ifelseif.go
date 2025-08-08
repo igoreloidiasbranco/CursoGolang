666995a7fb7f3398b93e16f7b124ee9e4481ee13
Igor Eloi <igor.dbranco@mercadolivre.com>
2025-08-08 15:11:37 -0300

    if elseif

---
+++ estruturacontroles/ifelseif/ifelseif.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func notaParaConceito(nota float64) string {
+
+	if nota >= 9 {
+		return "A"
+	} else if nota >= 8 && nota < 9 {
+		return "B"
+	} else if nota >= 5 && nota < 8 {
+		return "C"
+	} else if nota >= 3 && nota < 5 {
+		return "D"
+	} else {
+		return "E"
+	}
+}
+
+func main() {
+
+	fmt.Println(notaParaConceito(11.8))
+	fmt.Println(notaParaConceito(8.7))
+	fmt.Println(notaParaConceito(6.5))
+	fmt.Println(notaParaConceito(4.0))
+	fmt.Println(notaParaConceito(2.7))
+
+}
