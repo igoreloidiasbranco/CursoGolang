1852e5be84b5bcb6657a93d6118097443aaa23de
Igor Eloi <igor.dbranco@mercadolivre.com>
2025-08-07 21:30:31 -0300

    operadores lógicos

---
+++ fundamentos/operadoreslogicos/logicos.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func compras(trab1, trab2 bool) (bool, bool, bool) { // primeiro parenteses são os parâmetros(entrada), o segundo parenteses é o retorno(saída)
+
+	comprarTv50 := trab1 && trab2
+	comprarTv32 := trab1 != trab2 // ou exclusivo
+	comprarSorvete := trab1 || trab2
+
+	return comprarTv50, comprarTv32, comprarSorvete
+}
+
+func main() {
+
+	tv50, tv32, sorvete := compras(true, true)
+	fmt.Printf("Tv50: %t, Tv32: %t, Sorvete: %t, Saudável: %t", tv50, tv32, sorvete, !sorvete)
+
+}
