d61dcf642c80db751450d89567334d9b730d2326
Igor Eloi <igor.dbranco@mercadolivre.com>
2025-08-12 13:32:58 -0300

    função variática

---
+++ funcoes/funcaovariatica/funcaovariatica.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// funções variáticas recebem quantidade de parâmetros variáveis
+
+func media(numeros ...float64) float64 {
+
+	total := 0.0
+	for _, num := range numeros {
+		total += num
+	}
+
+	return total / float64(len(numeros))
+}
+
+func main() {
+
+	fmt.Printf("Média: %.2f", media(7.7, 8.1, 5.9))
+}
